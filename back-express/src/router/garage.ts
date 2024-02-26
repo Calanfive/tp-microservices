892e8090cd3c3cde05628e07e39e460795fc223d
calanfive <camille.lelan@gmail.com>
2024-02-26 16:53:58 +0100

    update delete

--- back-express/src/router/garage.ts
+++ back-express/src/router/garage.ts
@@ -12,6 +12,25 @@ garageRouter.post("/add", async (req, res) => {
     const {marque, modele, annee, couleur} = req.body;
     const newCar = await garage.create({marque, modele, annee, couleur});
     console.log(newCar);
-    
     res.json(newCar)
-});
+})
+
+garageRouter.put("/:id", async (req, res) => {
+    const SelectById = req.params.id
+    const modifById = await garage.findByPk(SelectById);
+    const modified = await modifById?.update({
+        marque: req.body.marque,
+        modele: req.body.modele,
+        annee: req.body.annee,
+        couleur: req.body.couleur
+    })
+    res.json(modified)
+    res.send('information modifiée')
+})
+
+garageRouter.delete("/:id", async (req, res) => {
+    const SelectById = req.params.id
+    const deleteById = await garage.findByPk(SelectById);
+    deleteById?.destroy()
+    res.send('supression voiture')
+})
